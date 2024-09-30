61bd66c8eed3bf516dab9bd9b598d6c2cc933e2f
Adjel <adjel.touaoula@gmail.com>
2024-09-30 04:40:53 +0200

    bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero/Hero"
+
 export const App = () => {
   return (
-    <section className="w-full h-full border-4 bg-black">
-      <div className="bg-[url('/assets/bg.webp')] w-full h-auto text-red-600 font-extrabold">casse les couilles</div>
+    <section className="w-full h-auto">
+      <Hero />
     </section>
   )
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section>
+      <div className="bg-[url('/assets/bg.webp')] w-full h-screen bg-cover bottom-92">o ia poke</div>
+    </section>
+  );
+}
+
+export default Hero;
